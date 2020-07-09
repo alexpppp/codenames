c97dc63856a6697098c09a3edb6b50cb6b48c25d
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-09 18:04:26 +1200

    make generateBoard work

--- src/index.js
+++ src/index.js
@@ -22,58 +22,22 @@ Game
 
 
 
-var words = ["NAPOLEON", "BLIND", "CHECK", "TANK", "SPURS", "SHEET", "QUEEN", "MARK", "GENIE", "WASHINGTON", "PIN", "MUD", "PARROT", "SHAKESPEARE", "ENGINE", "MEMORY", "JOCKEY", "FLUTE", "CARROT", "POCKET", "BATTLE", "PAINT", "KNOT", "GANGSTER", "BUTTERFLY", "FOG", "KICK"]
+var words = ["NAPOLEON", "BLIND", "CHECK", "TANK", "SPURS", "SHEET", "QUEEN", "MARK", "GENIE", "WASHINGTON", "PIN", "MUD", "PARROT", "SHAKESPEARE", "ENGINE", "MEMORY", "JOCKEY", "FLUTE", "CARROT", "POCKET", "BATTLE", "PAINT", "KNOT", "GANGSTER", "BUTTERFLY", "FOG", "KICK", "STRAW", "GEAR", "SWITCH", "BRIDE", "DRUM", "PALM", "MOSCOW", "FIRE", "BAND", "GREECE", "SQUARE", "NOVEL", "PIPE", "SMUGGLER", "ENGLAND", "ROSE", "FAN", "CHANGE", "OCTOPUS", "WEB", "WHIP", "STADIUM", "DRAGON", "MISSILE", "PASTE", "SPIKE", "SCIENTIST", "BLOCK", "FRANCE", "PIANO","BUCK", "SPOT", "TIME", "TORCH", "STATE", "AGENT", "PRINCESS", "ROULETTE"]
 
 var board = {
-    cards: [
-        {
-            rowCount: 0,
-            colCount: 0,
-            word: "BEIJING",
-            guessed: false,
-            tye: "civilian"
-        },
-        {
-            rowCount: 0,
-            colCount: 0,
-            word: "BEIJING",
-            guessed: false,
-            tye: "civilian"
-        },
-        {
-            rowCount: 0,
-            colCount: 0,
-            word: "BEIJING",
-            guessed: false,
-            tye: "civilian"
-        },
-        {
-            rowCount: 0,
-            colCount: 0,
-            word: "BEIJING",
-            guessed: false,
-            tye: "civilian"
-        },
-        {
-            rowCount: 0,
-            colCount: 0,
-            word: "WASHINGTON",
-            guessed: false,
-            tye: "civilian"
-        },
-    ]
+    cards: []
 }
 
 class Card extends React.Component {
     render() {
         return (
-            <div className="card brown">
+        <div className="card brown">
             <div className="box">
                 <div className="row">
-                    <h6 className="small">BEIJING</h6>
+                    <h6 className="small">{this.props.word}</h6>
                     <span className="profile"><img src={profile} alt="profile" /></span>
                 </div>
-                <h4 className="large">BEIJING</h4>
+                <h4 className="large">{this.props.word}</h4>
             </div>
         </div>
         )
@@ -127,14 +91,21 @@ class Map extends React.Component {
   }
   
   class Board extends React.Component {
-    
-  
     render() {
-      const status = 'Next player: X';
-  
+        console.log(this.props.board.cards)
+        var board = this.props.board.cards.map((card, index) => {
+            return (
+              <Card 
+                key={index} 
+                word={card.word} 
+                guessed={card.guessed}
+                secretColor={card.secretColor}
+                />
+            );
+          });
       return (
         <div className="row">
-            <Card />
+            {board}
         </div>
       );
     }
@@ -151,14 +122,38 @@ render() {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.generateBoard = this.generateBoard.bind(this);
+        this.state = {board: board};
+    }
+    generateBoard() {
+        let gameWords = []
+        for (let i = 0; i < 25; i++) {
+            let random = parseInt(Math.floor(Math.random() * i))
+            gameWords.push(words[random])
+            words.splice(parseInt(random), 1)
+        }
+        for (const i in gameWords) {
+            board.cards.push({
+                word: gameWords[i],
+                guessed: false,
+                secretColor: "blue"
+            })
+        }
+        this.setState({board: board});
+    }
     render() {
       return (
         <main>
             <h1>CODENAMES</h1>
-            <button onClick="generateBoard()">New Game</button>
-            <Board />
-            <Map />
-            <PrintJSON />
+            <button onClick={this.generateBoard}>New Game</button>
+            <div className="container">
+                <Board
+                board={this.props.board} />
+                <Map />
+                <PrintJSON />
+            </div>
         </main>
       );
     }
@@ -168,6 +163,5 @@ class Game extends React.Component {
   
   ReactDOM.render(
     <Game board={board}/>,
-    document.getElementById('root')
-  );
+    document.getElementById('root'));
   
