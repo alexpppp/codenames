4dabfe5805240ce3648cc2c04c49a455d2f06e89
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-13 16:59:27 +1200

    refactor all class components to functional components using useState

--- src/index.js
+++ src/index.js
@@ -3,30 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import profile from './static/images/profile.png'
 
-/*
-Structure
-
-Game
-    - Board
-        - Card
-    - Map
-    
-	- AddPerson component
-  - PeopleList component
-    - Person #1 component
-    - Person #2
-    ...
-    - Person #N
-	- GenerateMatch button and function within App component
-*/
-
-
-
 var words = ["NAPOLEON", "BLIND", "CHECK", "TANK", "SPURS", "SHEET", "QUEEN", "MARK", "GENIE", "WASHINGTON", "PIN", "MUD", "PARROT", "SHAKESPEARE", "ENGINE", "MEMORY", "JOCKEY", "FLUTE", "CARROT", "POCKET", "BATTLE", "PAINT", "KNOT", "GANGSTER", "BUTTERFLY", "FOG", "KICK", "STRAW", "GEAR", "SWITCH", "BRIDE", "DRUM", "PALM", "MOSCOW", "FIRE", "BAND", "GREECE", "SQUARE", "NOVEL", "PIPE", "SMUGGLER", "ENGLAND", "ROSE", "FAN", "CHANGE", "OCTOPUS", "WEB", "WHIP", "STADIUM", "DRAGON", "MISSILE", "PASTE", "SPIKE", "SCIENTIST", "BLOCK", "FRANCE", "PIANO","BUCK", "SPOT", "TIME", "TORCH", "STATE", "AGENT", "PRINCESS", "ROULETTE"]
-
-var board = {
-    cards: []
-}
 
 const Card = ({word}) => 
         <div className="card brown">
@@ -39,9 +16,7 @@ const Card = ({word}) =>
             </div>
         </div>
 
-class Map extends React.Component {
-    render() {
-      return (
+const Map = () =>
         <div className="map-section">
             <div className="map">
                 <div className="row">
@@ -81,82 +56,58 @@ class Map extends React.Component {
                 </div>
             </div>
         </div>
-      );
-    }
-  }
   
-  class Board extends React.Component {
-    render() {
-        console.log(this.props.board.cards)
-        var board = this.props.board.cards.map((card, index) => {
-            return (
-              <Card 
+ const Board = ({cardList}) =>
+            <div className="row">
+                {cardList.map((card, index) => (
+                <Card 
                 key={index} 
                 word={card.word} 
-                guessed={card.guessed}
-                secretColor={card.secretColor}
-                />
-            );
-          });
-      return (
-        <div className="row">
-            {board}
-        </div>
-      );
-    }
-  }
-
-
-class PrintJSON extends React.Component {
-render() {
-    return (
+                />  
+                ))}
+            </div>
+      
+const PrintJSON = ({cardList}) =>
     <div className="json">
-    {JSON.stringify(board)}</div>        
-    )
-    }
-}
+        {JSON.stringify(cardList)}
+    </div>
 
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.generateBoard = this.generateBoard.bind(this);
-        this.state = {board: board};
-    }
-    generateBoard() {
+function Game() {
+    const [cardList, setCardList] = useState([])
+
+    const generateBoard = () => {
         let gameWords = []
-        for (let i = 0; i < 25; i++) {
-            let random = parseInt(Math.floor(Math.random() * i))
-            gameWords.push(words[random])
-            words.splice(parseInt(random), 1)
-        }
-        for (const i in gameWords) {
-            board.cards.push({
-                word: gameWords[i],
-                guessed: false,
-                secretColor: "blue"
-            })
-        }
-        this.setState({board: board});
+            for (let i = 0; i < 25; i++) {
+                let random = parseInt(Math.floor(Math.random() * i))
+                gameWords.push(words[random])
+                words.splice(parseInt(random), 1)
+            }
+            var newCardList = []
+            for (const i in gameWords) {
+                newCardList.push({
+                    word: gameWords[i],
+                    guessed: false,
+                    secretColor: "blue"
+                })
+            }
+         setCardList(newCardList);
     }
-    render() {
-      return (
+    return (
         <main>
             <h1>CODENAMES</h1>
-            <button onClick={this.generateBoard}>New Game</button>
+            <button onClick={generateBoard}>New Game</button>
             <div className="container">
                 <Board
-                board={this.props.board} />
+                cardList={cardList} />
                 <Map />
-                <PrintJSON />
+                <PrintJSON
+                cardList={cardList} />
             </div>
         </main>
       );
-    }
-  }
+}
   
-  // ========================================
-  
-  ReactDOM.render(
-    <Game board={board}/>,
+ReactDOM.render(
+    <Game/>,
     document.getElementById('root'));
   
