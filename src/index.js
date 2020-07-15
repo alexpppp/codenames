47b9035cf79110ce2f0e10ee48741f4c368ea728
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-15 13:33:31 +1200

    call generate board once on initial  pageload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import profile from './static/images/profile.png'
@@ -68,6 +68,11 @@ function Game() {
         count: 1
     }
     ])
+
+    useEffect(()=>{
+        generateCardList()
+    }, [])
+
     const generateCardList = () => {
         let tempWords = []
         tempWords = words
