f3fe16841945fbc409d6fa7aa77a14261bf79976
Alex Procter-Panzer <acoprocter@gmail.com>
2020-07-17 13:13:49 +1200

    Updates

---
+++ precache-manifest.e6b38c23acc1f49d7f4af83a96191ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11077a70207713f6fa77eff77bec5c53",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "38b881e3b37d18ec8b1e",
+    "url": "/codenames/static/css/main.1cbc26b9.chunk.css"
+  },
+  {
+    "revision": "da2e1d5e698268c3debf",
+    "url": "/codenames/static/js/2.1969972c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codenames/static/js/2.1969972c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b881e3b37d18ec8b1e",
+    "url": "/codenames/static/js/main.3affdf08.chunk.js"
+  },
+  {
+    "revision": "2c1662ef0022e2790dde",
+    "url": "/codenames/static/js/runtime-main.4b7910aa.js"
+  }
+]);
